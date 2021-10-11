21f57b97a4f8202112a1ab8bc70ef0b730bf336b
sicoman <ahmedtheseer891996@gmail.com>
2021-10-11 11:26:29 +0200

    database backup finished

--- src/main/java/org/traccar/schedule/TaskTakeDatabaseBackup.java
+++ src/main/java/org/traccar/schedule/TaskTakeDatabaseBackup.java
@@ -22,10 +22,10 @@ import java.util.concurrent.TimeUnit;
 
 public class TaskTakeDatabaseBackup implements Runnable {
 
-    private static final long PERIOD_DAYS = 10;
+    private static final long PERIOD_DAYS = 30;
 
     public void schedule(ScheduledExecutorService executor) {
-        executor.scheduleAtFixedRate(this, PERIOD_DAYS, PERIOD_DAYS, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(this, PERIOD_DAYS, PERIOD_DAYS, TimeUnit.DAYS);
     }
 
     @Override
